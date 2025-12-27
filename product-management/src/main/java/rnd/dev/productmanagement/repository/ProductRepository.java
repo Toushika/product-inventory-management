e8e233940e3f858b912b9074a51aad38522bbc87
toushika111 <toushika.islam@s2021.tu-chemnitz.de>
2025-12-27 20:22:27 +0100

    Add logs ignoring logic in gitignore

--- product-management/src/main/java/rnd/dev/productmanagement/repository/ProductRepository.java
+++ product-management/src/main/java/rnd/dev/productmanagement/repository/ProductRepository.java
@@ -9,5 +9,13 @@ import rnd.dev.productmanagement.enums.ProductCategory;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
+
     Page<Product> findByProductCategory(ProductCategory productCategory, Pageable pageable);
+
+//    @Query("SELECT p FROM Product p WHERE p.price = (SELECT MAX(p2.price) FROM Product p2)")
+//    List<Product> findProductByMaxPrice();
+//
+//    @Query("SELECT p FROM Product p WHERE p.price = (SELECT Min(p2.price) FROM Product p2)")
+//    List<Product> findProductByMinPrice();
+
 }
